8f47dce63a3e1a7f6e5bcdfd99cf6c055b4c278b
eleuvete <villa@vecinapp.org>
2024-06-27 19:37:03 -0600

    update header

--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,23 +1,23 @@
 <template>
-  <section class="header">
-    <div class="header-logo">
-      <SVGLogo />
-    </div>
-  </section>
+	<section class="header">
+		<div class="header-logo">
+			<SVGLogo />
+		</div>
+	</section>
 </template>
 
 <style>
 .header {
-  width: 100%;
-  padding: 0.7em;
+	width: 100%;
+	padding: 0.7em;
 }
 .header-logo {
-  width: 130px;
+	width: 130px;
 }
 
-@media (min-width: 800) {
-  .header-logo {
-    width: 180px;
-  }
+@media (min-width: 800px) {
+	.header-logo {
+		width: 180px;
+	}
 }
 </style>
